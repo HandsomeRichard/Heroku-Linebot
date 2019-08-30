d9a87d7ce8178ac18df457c3686940b3d3d0f3ef
Richard Hsieh <richard980445@gmail.com>
2019-08-30 17:40:37 +0800

    fix Linetest.py

--- Linetest.py
+++ Linetest.py
@@ -37,7 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+    content = "{}: {}".format(event.source.user_id, event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=content))
 
 
 import os
