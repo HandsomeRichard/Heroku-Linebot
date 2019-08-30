e469c3ee1fb03c9c6e4bfd57e0deff9accc3faea
Richard Hsieh <richard980445@gmail.com>
2019-08-30 18:04:30 +0800

    fix message2

--- Linetest.py
+++ Linetest.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.replyToken,
+        event.reply_token,
         TextSendMessage(text=event.message.text))
 
 
