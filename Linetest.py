f598886334e965c5d4d05b16a787570256a7b402
Richard Hsieh <richard980445@gmail.com>
2019-09-02 14:19:22 +0800

    API test FollowEvent

--- Linetest.py
+++ Linetest.py
@@ -60,7 +60,6 @@ def handle_message(event):
 
 @handler.add(FollowEvent)
 def handle_follow(event):
-    
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text='Got follow event'))
 
