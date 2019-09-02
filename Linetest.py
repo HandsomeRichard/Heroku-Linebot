2416cf4544a0f99c5d884d67a8e5a513dc9d0497
HandsomeRichard <richard980445@gmail.com>
2019-09-02 11:15:05 +0800

    Update Linetest.py

--- Linetest.py
+++ Linetest.py
@@ -38,6 +38,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("richard OKOK")
+    line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
 
 
 import os
